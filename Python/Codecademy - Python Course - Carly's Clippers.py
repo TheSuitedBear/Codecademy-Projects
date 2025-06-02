fb3cc6fb20ef755beb582b7c3a0a410e71f9a818
Callum Smith <callumsmith1315.business@gmail.com>
2025-06-02 14:03:20 +0100

    Refactor print statements for clarity in Carly's Clippers program

--- Python/Codecademy - Python Course - Carly's Clippers.py
+++ Python/Codecademy - Python Course - Carly's Clippers.py
@@ -11,7 +11,7 @@ average_price = total_price / len(prices)
 print("Average Haircut Price: " + str(average_price))
 
 new_prices = [new - 5 for new in prices]
-print(new_prices)
+print("These are the new prices for all our haircuts: " + str(new_prices))
 
 total_revenue = 0
 for i in range(len(hairstyles)):
@@ -19,7 +19,7 @@ for i in range(len(hairstyles)):
 print("Total Revenue: " + str(total_revenue))
 
 average_daily_revenue = total_revenue / 7
-print(average_daily_revenue)
+print("This is the average daily revenue" + str(average_daily_revenue))
 
 cuts_under_30 = [hairstyles[i] for i in range(len(new_prices)) if new_prices[i] <= 30]
-print(cuts_under_30)
+print("Here is our haircuts that are under 30 dollars: " + str(cuts_under_30))
