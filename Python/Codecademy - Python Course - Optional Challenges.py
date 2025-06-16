399b4273e01c573b04072393f5dfc6a07fa2e9bf
Callum Smith <callumsmith1315.business@gmail.com>
2025-06-16 16:32:14 +0100

    Added multiple optional challenges with my own solutions:
    - a function that checks if an input is within a range of numbers
    - a function that checks if 2 names are identical and returns true if so
    - a function which is an intentional contradiction, always returning false
    - a function which returns different statements about a movie depending on its numerical rating
    - a function that picks the largest out of 3 numbers, and notes any ties that occur.

--- Python/Codecademy - Python Course - Optional Challenges.py
+++ Python/Codecademy - Python Course - Optional Challenges.py
@@ -79,3 +79,94 @@ print(divisible_by_ten(20))
 # Should result in True
 print(divisible_by_ten(25))
 # Should result in False
+
+
+
+# Function takes 3 inputs, a number, and 2 "range" numbers, and then returns true or false if the first number is within the range of the other 2 numbers
+
+def in_range(num, lower, upper):
+  if num >= lower and num <= upper:
+    return True
+  else:
+    return False
+
+print(in_range(10, 10, 10))
+# should print True
+# First number is the number being checked, the second number is the lower value, and the third is the higher value.
+print(in_range(5, 10, 20))
+# should print False
+
+
+
+# Checks if 2 names are identical and returns true if so, if not, then returns false
+
+def same_name(your_name, my_name):
+  if str(your_name) == str(my_name):
+    return True
+  else:
+    return False
+print(same_name("Colby", "Colby"))
+# should print True
+print(same_name("Tina", "Amber"))
+# should print False
+
+
+
+# A function which is an intentional contradiction, returning false no matter what, true cannot be achieved.
+
+def always_false(num):
+  if num > num or num < num:
+    return True
+  else:
+    return False
+
+print(always_false(0))
+# should print False
+print(always_false(-1))
+# should print False
+print(always_false(1))
+# should print False
+
+
+
+# A movie rating function which returns a response based on a number inputted between 1 - 10, I also added a response to any numbers outside of the listed range.
+
+def movie_review(rating):
+  if int(rating) <= 5 and int(rating) >= 1:
+    return "Avoid at all costs!"
+  elif int(rating) > 5 and int(rating) < 9:
+    return "This one was fun."
+  elif int(rating) >= 9 and int(rating) < 11:
+    return "Outstanding!"
+  else:
+    return "Rating is outside regular parameters, the number should be between 1 and 10"
+
+print(movie_review(9))
+# should print "Outstanding!"
+print(movie_review(4))
+# should print "Avoid at all costs!"
+print(movie_review(6))
+# should print "This one was fun."
+
+
+
+# Checks which number out of a series of 3 inputs is the largest number, if the top number is tied with another number, it returns "It's a tie!"
+
+def max_num(num1, num2, num3):
+  if num1 > num2 and num1 > num3:
+    return num1
+  elif num2 > num1 and num2 > num3:
+    return num2
+  elif num3 > num1 and num3 > num2:
+    return num3
+  else:
+    return "It's a tie!"
+
+print(max_num(-10, 0, 10))
+# should print 10
+print(max_num(-10, 5, -30))
+# should print 5
+print(max_num(-5, -10, -10))
+# should print -5
+print(max_num(2, 3, 3))
+# should print "It's a tie!"
