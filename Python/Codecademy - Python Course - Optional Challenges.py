840caa0dd4692e4dd97281a923789c56ba7f99b2
Callum Smith <callumsmith1315.business@gmail.com>
2025-07-12 09:02:55 +0100

    Added new challenges and solutions involving functions for calculating multiples, tips, introductions, dog years, and performing complex math operations with corresponding test cases.

--- Python/Codecademy - Python Course - Optional Challenges.py
+++ Python/Codecademy - Python Course - Optional Challenges.py
@@ -496,3 +496,85 @@ print(remainder(15, 14))
 # Should return 2
 print(remainder(9, 6))
 # Should return 0
+
+
+
+# Takes an input and prints the first 3 multiples of the input, then returns the 3rd multiple
+
+def first_three_multiples(num):
+  print(num * 1)
+  print(num * 2)
+  print(num * 3)
+  return num * 3
+
+first_three_multiples(10)
+# Should print 10, 20, 30, and return 30
+first_three_multiples(0)
+# Should print 0, 0, 0, and return 0
+
+# Alternate version
+
+def first_three_multiples_alt(num):
+  for i in range(1, 4):
+    print(num * i)
+    i += 1
+  return num * 3
+
+first_three_multiples_alt(9)
+
+
+
+# Takes 2 values, one being the cost of a meal, and the other the percentage tip you'd like to leave, and the function calculates how much you would need to pay.
+
+def tip(total, percentage):
+  total_tip = (total * percentage) / 100
+  return total_tip
+
+print(tip(10, 25))
+# Should print 2.5
+print(tip(0, 100))
+# Should print 0.0
+
+
+
+# Recreates the iconic "Bond, James Bond" quote with any first and last name as an input
+
+def introduction(first_name, last_name):
+  return last_name + ", " + first_name + " " + last_name
+
+print(introduction("James", "Bond"))
+# should print Bond, James Bond
+print(introduction("Maya", "Angelou"))
+# should print Angelou, Maya Angelou
+print(introduction("Callum", "Smith"))
+
+
+
+# Takes a name and age, and calculates how old they would be in dog years
+
+def dog_years(name, age):
+  dog_age = age * 7
+  return name + ", you are " + str(dog_age) + " years old in dog years"
+
+print(dog_years("Lola", 16))
+# should print "Lola, you are 112 years old in dog years"
+print(dog_years("Baby", 0))
+# should print "Baby, you are 0 years old in dog years"
+
+
+
+# Takes 4 numbers, adds the first 2 together, subtracts the final 2, then multiplies the results together and then gets the remainder of the total from the first value
+
+def lots_of_math(a, b, c, d):
+   a_b = a + b
+   print(a_b)
+   c_d = c - d
+   print(c_d)
+   total = a_b * c_d
+   print(total)
+   return total % a
+
+print(lots_of_math(1, 2, 3, 4))
+# should print 3, -1, -3, 0
+print(lots_of_math(1, 1, 1, 1))
+# should print 2, 0, 0, 0
