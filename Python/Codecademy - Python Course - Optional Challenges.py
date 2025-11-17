36774b6444bdca329958b3444d94ee2939db7699
Callum Smith <callumsmith1315.business@gmail.com>
2025-11-17 02:31:00 +0000

    Added more optional challenges involving functions

--- Python/Codecademy - Python Course - Optional Challenges.py
+++ Python/Codecademy - Python Course - Optional Challenges.py
@@ -666,3 +666,100 @@ print(x_length_words("i like apples", 2))
 # should print False
 print(x_length_words("he likes apples", 2))
 # should print True
+
+
+
+# Function takes a greeting/sentence and a name and returns true if the name is within the sentence.
+
+def check_for_name(sentence, name):
+  split_sentence = sentence.split(" ")
+  for word in split_sentence:
+    if word.lower() == name.lower():
+      return True
+    else:
+      continue
+  return False
+
+print(check_for_name("My name is Jamie", "Jamie"))
+# should print True
+print(check_for_name("My name is jamie", "Jamie"))
+# should print True
+print(check_for_name("My name is Samantha", "Jamie"))
+# should print False
+
+# This can also be simplified into one line
+
+def check_for_name_simple(sentence, name):
+  return name.lower() in sentence.lower()
+
+print(check_for_name_simple("My name is Jamie", "Jamie"))
+# should print True
+print(check_for_name_simple("My name is jamie", "Jamie"))
+# should print True
+print(check_for_name_simple("My name is Samantha", "Jamie"))
+# should print False
+
+
+
+# Function takes a string and puts every other letter into a new string and returns it
+
+def every_other_letter(word):
+  every_other = ""
+  for i in range(0, len(word), 2):
+    every_other += word[i]
+  return every_other
+
+print(every_other_letter("Codecademy"))
+# should print Cdcdm
+print(every_other_letter("Hello world!"))
+# should print Hlowrd
+print(every_other_letter(""))
+# should print
+
+
+
+# Reverses a string and returns it
+
+def reverse_string(word):
+  reversed_string = ""
+  for i in range(len(word)-1, -1, -1):
+    reversed_string += word[i]
+  return reversed_string
+
+print(reverse_string("Codecademy"))
+# should print ymedacedoC
+print(reverse_string("Hello world!"))
+# should print !dlrow olleH
+print(reverse_string(""))
+# should print
+
+
+
+# Swaps the first letter of 2 words and returns the 2 words as a sentence
+
+def make_spoonerism(word1, word2):
+  new_word1 = word2[0] + word1[1:]
+  new_word2 = word1[0] + word2[1:]
+  new_sentence = new_word1 + " " + new_word2
+  return new_sentence
+
+print(make_spoonerism("Codecademy", "Learn"))
+# should print Lodecademy Cearn
+print(make_spoonerism("Hello", "world!"))
+# should print wello Horld!
+print(make_spoonerism("a", "b"))
+# should print b a
+
+
+
+# Keeps adding ! to a string if the string is under 20 characters, once the string is 20 characters or more, it is returned
+
+def add_exclamation(word):
+  while(len(word) < 20):
+    word += "!"
+  return word 
+
+print(add_exclamation("Codecademy"))
+# should print Codecademy!!!!!!!!!!
+print(add_exclamation("Codecademy is the best place to learn"))
+# should print Codecademy is the best place to learn
